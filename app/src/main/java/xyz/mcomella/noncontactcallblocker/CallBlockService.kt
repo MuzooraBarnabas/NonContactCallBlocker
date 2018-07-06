6fe7e3ae9e152f35c37b2189c3cea6debf6b78c8
Michael Comella <michael.l.comella@gmail.com>
2018-07-06 13:22:56 -0700

    Add CallBlockService; block calls.

---
+++ app/src/main/java/xyz/mcomella/noncontactcallblocker/CallBlockService.kt
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package xyz.mcomella.noncontactcallblocker
+
+import android.net.Uri
+import android.telecom.Call
+import android.telecom.CallScreeningService
+import android.telecom.TelecomManager
+
+/** The call blocking logic in the app. */
+class CallBlockService : CallScreeningService() {
+
+    override fun onScreenCall(callDetails: Call.Details) {
+        val number = callDetails.intentExtras[TelecomManager.EXTRA_INCOMING_CALL_ADDRESS] as Uri? // tel:...
+        val isCallBlocked = if (number == null) {
+            true // It's an assumption this is an unknown number, but we want to block unknown numbers.
+        } else {
+            !Contacts.isNumberInContacts(contentResolver, number)
+        }
+
+        respondToCall(callDetails, getCallResponse(isCallBlocked))
+        if (isCallBlocked) {
+            CallBlockLogModel.storeBlockedCall(number)
+        }
+    }
+
+    private fun getCallResponse(isCallBlocked: Boolean): CallResponse {
+        val callResponseBuilder = CallResponse.Builder()
+                .setDisallowCall(isCallBlocked)
+        if (isCallBlocked) {
+            callResponseBuilder
+                    .setSkipCallLog(true)
+                    .setSkipNotification(true)
+        }
+        return callResponseBuilder.build()
+    }
+}
