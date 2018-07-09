c34e274b799de63dffb3f31fd1e2c09849e50865
Michael Comella <michael.l.comella@gmail.com>
2018-07-09 09:17:07 -0700

    Add GPL license headers, copyright to kt files.

--- app/src/main/java/xyz/mcomella/noncontactcallblocker/CallBlockService.kt
+++ app/src/main/java/xyz/mcomella/noncontactcallblocker/CallBlockService.kt
@@ -1,3 +1,21 @@
+/* Copyright (C) 2018 Michael Comella
+ *
+ * This file is part of NonContactCallBlocker.
+ *
+ *  NonContactCallBlocker is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  NonContactCallBlocker is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with NonContactCallBlocker.  If not, see
+ *  <https://www.gnu.org/licenses/>. */
+
 package xyz.mcomella.noncontactcallblocker
 
 import android.net.Uri
