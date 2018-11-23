62169f2a688c8c162b645db073759c0bf2051c89
Michael Comella <michael.l.comella@gmail.com>
2018-11-23 13:01:56 -0800

    Issue #26: Add LiveData.testValue.
    This calls through to the thirdparty LiveDataTestUtil.

---
+++ app/src/androidTest/java/xyz/mcomella/noncontactcallblocker/helpers/ext/LiveData.kt
@@ -0,0 +1,30 @@
+/* Copyright (C) 2018 Michael Comella
+ *
+ * This file is part of NonContactCallBlocker.
+ *
+ *  NonContactCallBlocker is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  NonContactCallBlocker is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with NonContactCallBlocker.  If not, see
+ *  <https://www.gnu.org/licenses/>. */
+
+package xyz.mcomella.noncontactcallblocker.helpers.ext
+
+import android.arch.lifecycle.LiveData
+import com.android.example.github.util.LiveDataTestUtil
+
+/**
+ * Waits for the [LiveData.getValue] to be updated and returns the result: intended to replace
+ * [LiveData.getValue] during tests.
+ */
+fun <T> LiveData<T>.testValue(): T {
+    return LiveDataTestUtil.getValue(this)
+}
